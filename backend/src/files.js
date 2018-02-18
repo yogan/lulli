3fdb5002462c4166cd64f378f50567ade066a655
Frank Blendinger <fb@intoxicatedmind.net>
2018-02-18 20:36:34 +0100

    backend/src/files.js: internal restructoring

--- backend/src/files.js
+++ backend/src/files.js
@@ -1,7 +1,29 @@
 const fs     = require('fs');
 const config = require('./config');
 
-const rootPath = config.getPath();
+module.exports.search = search;
+
+function search(searchTerms) {
+  const rootPath = config.getPath();
+  const subdirs = getSubdirs(rootPath);
+
+  const dirsWithImages = subdirs.map(dir => ({
+    dir,
+    images: getMatchingImages(`${rootPath}/${dir}`, searchTerms)
+  }));
+
+  return flattenImageResults(dirsWithImages);
+}
+
+function getSubdirs(path) {
+  return fs.readdirSync(path)
+    .filter(filename => fs.statSync(`${path}/${filename}`).isDirectory());
+}
+
+function getMatchingImages(path, searchTerms) {
+  const filenames = listImageFiles(path);
+  return filenames.filter(filename => allTermsMatch(filename, searchTerms));
+}
 
 function listImageFiles(path) {
   return fs
@@ -19,32 +41,9 @@ function allTermsMatch(filename, searchTerms) {
   }, true);
 }
 
-function getSubdirs(path) {
-  return fs.readdirSync(path)
-    .filter(filename => fs.statSync(`${path}/${filename}`).isDirectory());
-}
-
-function search(searchTerms) {
-  const subdirs = getSubdirs(rootPath);
-
-  const dirsWithImages = subdirs.map(dir => ({
-    dir,
-    images: getMatchingImages(`${rootPath}/${dir}`, searchTerms)
-  }));
-
-  return flattenImageResults(dirsWithImages);
-}
-
 function flattenImageResults(dirsWithImages) {
   return dirsWithImages.reduce((acc, cur) => {
     cur.images.forEach(image => acc.push(`${cur.dir}/${image}`));
     return acc;
   }, []);
 }
-
-function getMatchingImages(path, searchTerms) {
-  const filenames = listImageFiles(path)
-  return filenames.filter(filename => allTermsMatch(filename, searchTerms));
-}
-
-module.exports.search = search;
