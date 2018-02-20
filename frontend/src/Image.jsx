614cb90a5c769a74bff42e5771cf2b6d87cd22e2
Frank Blendinger <fb@intoxicatedmind.net>
2018-02-20 22:09:34 +0100

    better titles for images
    - include year
    - strip file extension
    - replace underscores with spaces

--- frontend/src/Image.jsx
+++ frontend/src/Image.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export default function Image({image}) {
-  const {filename, url} = image;
-  return <img src={url} alt={filename} title={filename} />;
+  const {filename, url, year} = image;
+  const title = makeTitle(filename, year);
+  return <img src={url} alt={title} title={title} />;
+}
+
+function makeTitle(filename, year) {
+  const name = filename.replace(/\.[^.]*$/, '');
+  const withSpaces = name.replace(/_/g, ' ');
+  return `${withSpaces} (${year})`;
 }
