cc6e258be92193864827393c1481c80177d5093d
Frank Blendinger <fb@intoxicatedmind.net>
2018-02-23 22:30:12 +0100

    add arraysAreEqual() function to utils (/w tests)

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,7 +1,20 @@
 // @ts-check
 module.exports = {
+  arraysAreEqual,
   flatten
 };
+
+function arraysAreEqual(leftArray, rightArray) {
+  const leftIsInRight = leftArray.reduce((acc, cur) => (
+    acc && rightArray.includes(cur)
+  ), true);
+
+  const rightIsInLeft = rightArray.reduce((acc, cur) => (
+    acc && leftArray.includes(cur)
+  ), true);
+
+  return leftIsInRight && rightIsInLeft;
+}
 
 function flatten(array) {
   return [].concat(...array);
--- backend/src/utils.test.js
+++ backend/src/utils.test.js
@@ -1,7 +1,55 @@
 // @ts-check
 import test from 'ava';
 
-import {flatten} from './utils';
+import {
+  arraysAreEqual,
+  flatten
+} from './utils';
+
+test('arraysAreEqual() returns true for two empty arrays', t => {
+  const array1 = [];
+  const array2 = [];
+
+  const result = arraysAreEqual(array1, array2);
+
+  t.true(result);
+});
+
+test('arraysAreEqual() returns true for two arrays with same elements in same order', t => {
+  const array1 = [1, 'a', 'bar'];
+  const array2 = [1, 'a', 'bar'];
+
+  const result = arraysAreEqual(array1, array2);
+
+  t.true(result);
+});
+
+test('arraysAreEqual() returns true for two arrays with same elements in different order', t => {
+  const array1 = [1, 'a', 'bar'];
+  const array2 = ['a', 1, 'bar'];
+
+  const result = arraysAreEqual(array1, array2);
+
+  t.true(result);
+});
+
+test('arraysAreEqual() returns false for an empty and an non empty array', t => {
+  const array2 = [];
+  const array1 = [1];
+
+  const result = arraysAreEqual(array1, array2);
+
+  t.false(result);
+});
+
+test('arraysAreEqual() returns false for a non empty and an empty array', t => {
+  const array1 = [1];
+  const array2 = [];
+
+  const result = arraysAreEqual(array1, array2);
+
+  t.false(result);
+});
 
 test('flatten() flattens an array of arrays', t => {
   const arrayOfArrays = [
