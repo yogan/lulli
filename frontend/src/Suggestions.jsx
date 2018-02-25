626554bbf4f408df6dbe850587643836f4b497e0
Frank Blendinger <fb@intoxicatedmind.net>
2018-02-25 16:03:53 +0100

    limit suggestions to 10, add message

--- frontend/src/Suggestions.jsx
+++ frontend/src/Suggestions.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 
 import './Suggestions.css';
 
+const MAX_SUGGESTIONS = 10;
+
 export function Suggestions({suggestions}) {
   if (!suggestions || suggestions.length === 0) {
     return null;
   }
 
-  const entries = suggestions.map((sugg, idx) =>
-    <li key={idx}>{sugg.filename}</li>);
+  const entries = suggestions
+    .slice(0, MAX_SUGGESTIONS)
+    .map((sugg, idx) => <li key={idx}>{sugg.filename}</li>);
+
+  const moreMatches = suggestions.length - MAX_SUGGESTIONS;
+  const moreMatchesNote = moreMatches > 0
+    ? <i>({moreMatches} more matches)</i>
+    : null;
 
   return (
     <ul className="suggestions">
       {entries}
+      {moreMatchesNote}
     </ul>
   );
 }
