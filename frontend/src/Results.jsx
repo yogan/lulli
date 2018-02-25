9a04d8c57c40459e37f641639df6f21f0c855c80
Frank Blendinger <fb@intoxicatedmind.net>
2018-02-25 15:56:35 +0100

    limit media results to 100, add message

--- frontend/src/Results.jsx
+++ frontend/src/Results.jsx
@@ -5,6 +5,8 @@ import './Results.css';
 import TitledMedia from './TitledMedia';
 import { newestTimestampsFirst } from './utils';
 
+const MAX_RESULTS = 100;
+
 export function Results({matches}) {
   if (!matches || matches.length === 0) {
     return <p>Nothing found. Oh no.</p>;
@@ -12,12 +14,21 @@ export function Results({matches}) {
 
   matches.sort((left, right) => newestTimestampsFirst(left, right));
 
-  const media = matches.map((match, idx) =>
+  const limitedMatches = matches.slice(0, MAX_RESULTS);
+
+  const resultsMissing = matches.length > MAX_RESULTS
+    ? <p className="results-missing">
+        {`Results limited to the first ${MAX_RESULTS} matches.`}
+      </p>
+    : null;
+
+  const media = limitedMatches.map((match, idx) =>
     <TitledMedia match={match} key={idx} />);
 
   return (
     <div className="results">
       {media}
+      {resultsMissing}
     </div>
   );
 }
