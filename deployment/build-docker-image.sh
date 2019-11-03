cc29196ab484d7d7b727b6c9faba304886851d57
Frank Blendinger <yogan@zogan.de>
2019-11-03 18:18:22 +0100

    Do not skip frontend build on --skip-npm

--- deployment/build-docker-image.sh
+++ deployment/build-docker-image.sh
@@ -4,7 +4,9 @@ if [[ "$1" == "--skip-npm" ]] ; then
   echo "Skipping npm ci, docker image will be built with current node_modules."
 else
   (cd backend  && npm ci && tar cf node_modules.tar node_modules) && \
-  (cd frontend && npm ci && npm run build)
+  (cd frontend && npm ci)
 fi
+
+(cd frontend && npm run build)
 
 docker build -t yogan/lulli .
